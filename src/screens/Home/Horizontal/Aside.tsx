7de75cf3eb4449bf1ef6ca66a438cb6f1108c750
x-music <x-music@example.com>
2026-05-02 03:09:08 +0000

    fix: 修改应用名称显示 - X Music

--- src/screens/Home/Horizontal/Aside.tsx
+++ src/screens/Home/Horizontal/Aside.tsx
@@ -70,7 +70,7 @@ const Header = () => {
     <View style={{ paddingTop: statusBarHeight }}>
       <View style={styles.header}>
         <Icon name="logo" color={theme['c-primary-dark-100-alpha-300']} size={22} />
-        {/* <Text style={styles.headerText} size={16} color={theme['c-primary-dark-100-alpha-300']}>LX Music</Text> */}
+        {/* <Text style={styles.headerText} size={16} color={theme['c-primary-dark-100-alpha-300']}>X Music</Text> */}
       </View>
     </View>
   )
